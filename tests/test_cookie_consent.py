34e0ff134484db0a29b0c76c69411a087675e97e
Jacek Majerski <jacekmajerskiqa@gmail.com>
2025-06-16 21:45:07 +0200

    fix workflow

--- tests/test_cookie_consent.py
+++ tests/test_cookie_consent.py
@@ -1,28 +1,35 @@
 import pytest
-from playwright.sync_api import expect, TimeoutError
+from playwright.sync_api import expect, TimeoutError, sync_playwright
 from pages.cookie_settings_page import CookieSettingsPage
 
 def test_accep(page):
     page.goto("https://www.pracuj.pl/")
     expect(page.locator('[data-test="button-submitCookie"]')).to_be_visible(timeout=60000)
 
+def test_anty_antybot():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            locale="pl-PL"
+        )
+        page = context.new_page()
+        page.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        page.goto("https://www.ing.pl", timeout=60000)
 
-def test_debug_ing_home(page):
-    page.goto("https://www.ing.pl", timeout=60000)
-    page.wait_for_selector("body", timeout=60000)
+        # "Ludzkie" zachowanie
+        page.mouse.move(100, 100)
+        page.wait_for_timeout(1000)
+        page.mouse.move(150, 200)
+        page.wait_for_timeout(1000)
 
-    # Screenshot
-    page.screenshot(path="debug_screenshot.png", full_page=True)
+        page.screenshot(path="antybot_screenshot.png", full_page=True)
+        print("Tytuł strony:", page.title())
 
-    # Dump HTML
-    with open("debug_dom.html", "w", encoding="utf-8") as f:
-        f.write(f"<h1>{page.title()}</h1><p>{page.url}</p>")
-        f.write(page.content())
+        context.close()
+        browser.close()
 
-    print("Tytuł strony:", page.title())
-    body_text = page.locator("body").inner_text()
-    print("BODY TEXT:", body_text[:500])
-    assert "cookie" in body_text.lower() or "logowanie" in body_text.lower()
 
 
 def test_accept_analytics_cookie(page):
