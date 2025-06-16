e71b2caf9282807e66016a19e0c3f1a8982f1d5b
Jacek Majerski <jacekmajerskiqa@gmail.com>
2025-06-16 22:00:37 +0200

    fix workflow

--- tests/test_cookie_consent.py
+++ tests/test_cookie_consent.py
@@ -6,35 +6,9 @@ def test_accep(page):
     page.goto("https://www.pracuj.pl/")
     expect(page.locator('[data-test="button-submitCookie"]')).to_be_visible(timeout=60000)
 
-def test_anty_antybot():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-            locale="pl-PL"
-        )
-        page = context.new_page()
-        page.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        page.goto("https://www.ing.pl", timeout=60000)
-
-        # "Ludzkie" zachowanie
-        page.mouse.move(100, 100)
-        page.wait_for_timeout(1000)
-        page.mouse.move(150, 200)
-        page.wait_for_timeout(1000)
-
-        page.screenshot(path="antybot_screenshot.png", full_page=True)
-        print("Tytuł strony:", page.title())
-
-        context.close()
-        browser.close()
-
-
 
 def test_accept_analytics_cookie(page):
     page.goto("https://www.ing.pl", timeout=60000)
-    page.wait_for_selector("#login-desktop", timeout=60000)
 
     print("TITLE:", page.title())
     print("URL:", page.url)
@@ -62,3 +36,28 @@ def test_accept_analytics_cookie(page):
     policy_cookie = next((c for c in cookies if c["name"] == "cookiePolicyGDPR"), None)
     assert policy_cookie is not None, "Brak ciasteczka 'cookiePolicyGDPR'."
     assert policy_cookie["value"] == "3", f"Oczekiwano 'cookiePolicyGDPR' z wartością '3', otrzymano: {policy_cookie['value']}"
+
+
+def test_anty_antybot():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            locale="pl-PL"
+        )
+        page = context.new_page()
+        page.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        page.goto("https://www.ing.pl", timeout=60000)
+
+        # "Ludzkie" zachowanie
+        page.mouse.move(100, 100)
+        page.wait_for_timeout(1000)
+        page.mouse.move(150, 200)
+        page.wait_for_timeout(1000)
+
+        page.screenshot(path="antybot_screenshot.png", full_page=True)
+        print("Tytuł strony:", page.title())
+
+        context.close()
+        browser.close()
