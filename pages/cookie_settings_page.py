886ad9669d7260722d731b68eb357b19989ae5bc
Jacek Majerski <jacekmajerskiqa@gmail.com>
2025-06-19 12:36:59 +0200

    add gitignore, change pages file

--- pages/cookie_settings_page.py
+++ pages/cookie_settings_page.py
@@ -1,16 +1,19 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, Locator
 
 class CookieSettingsPage:
     def __init__(self, page: Page):
         self.page = page
-
-    def accept_analytics_and_confirm(self):
-        analytics_checkbox = self.page.get_by_label("Analityczne")
-        if not analytics_checkbox.is_checked():
-            analytics_checkbox.check()
-        self.page.get_by_role("button", name="Zaakceptuj zaznaczone").click()
+        # Buttony
+        self.customize_button: Locator = page.get_by_role("button", name="Dostosuj")
+        self.accept_button: Locator = page.get_by_role("button", name="Zaakceptuj zaznaczone")
+        # Inne lokatory
+        self.analytics_checkbox: Locator = page.get_by_label("Analityczne")
 
     def open_custom_settings(self):
-        customize_button = self.page.get_by_role("button", name="Dostosuj")
-        customize_button.wait_for(timeout=60000)
-        customize_button.click()
+        self.customize_button.wait_for(timeout=60000)
+        self.customize_button.click()
+
+    def accept_analytics_and_confirm(self):
+        if not self.analytics_checkbox.is_checked():
+            self.analytics_checkbox.check()
+        self.accept_button.click()
